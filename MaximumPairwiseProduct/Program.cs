5bf21aa9e3f6a2d3a2e62a95f3116adf272361f2
epikkoder <ronaldsahagun87@gmail.com>
2018-09-29 18:23:32 -0700

    Added MaxPairwiseProductFast function

--- MaximumPairwiseProduct/Program.cs
+++ MaximumPairwiseProduct/Program.cs
@@ -10,21 +10,28 @@ namespace MaximumPairwiseProduct
 	{
 		static void Main(string[] args)
 		{
-			int product = MaxPairwiseProductNaive();
-
-			Console.WriteLine("Maximum product is " + product);
-		}
-
-		public static int MaxPairwiseProductNaive()
-		{
 			int inputCount = Convert.ToInt32(Console.ReadLine());
 			int[] numbers = new int[inputCount];
-			int product = 0;
 
 			for (int i = 0; i < inputCount; i++)
 			{
 				numbers[i] = Convert.ToInt32(Console.ReadLine());
 			}
+
+			Console.WriteLine("Running MaxPairwiseProductNaive...");
+			int product = MaxPairwiseProductNaive(numbers);
+			Console.WriteLine("Maximum product is " + product);
+
+			Console.WriteLine();
+
+			Console.WriteLine("Running MaxPairwiseProductFast...");
+			product = MaxPairwiseProductFast(numbers);
+			Console.WriteLine("Maximum product is " + product);
+		}
+
+		public static int MaxPairwiseProductNaive(int[] numbers)
+		{
+			int product = 0;
 
 			if (numbers.Length > 0)
 			{
@@ -41,6 +48,29 @@ namespace MaximumPairwiseProduct
 			}
 
 			return product;
+		}
+
+		public static int MaxPairwiseProductFast(int[] numbers)
+		{
+			int index1 = 0;
+			for (int i = 1; i < numbers.Length; i++)
+			{
+				if (numbers[i] > numbers[index1])
+				{
+					index1 = i;
+				}
+			}
+
+			int index2 = 0;
+			for (int i = 0; i < numbers.Length; i++)
+			{
+				if (numbers[i] != numbers[index1] && numbers[i] > numbers[index2])
+				{
+					index2 = i;
+				}
+			}
+
+			return numbers[index1] * numbers[index2];
 		}
 	}
 }
